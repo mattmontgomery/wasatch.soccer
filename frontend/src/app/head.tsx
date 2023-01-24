4599faf75a52cbf071b78ed4a8002e3567a394ca
Matt Montgomery <mmontgomery@deseretdigital.com>
2023-01-23 20:37:01 -0700

    feat: adds google analytics

--- frontend/src/app/head.tsx
+++ frontend/src/app/head.tsx
@@ -1,4 +1,5 @@
 import Config, { getConfig } from "@/app/util/config";
+import Script from "next/script";
 import DefaultTags from "./DefaultTags";
 import { getTitle } from "./util/site";
 
@@ -9,6 +10,22 @@ export default async function Head() {
       <DefaultTags />
       <title>{await getTitle([config.homepageTitleText], true)}</title>
       <meta name="description" content={config.siteDescription} />
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PRYEH8MYJ1"
+        strategy="afterInteractive"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-PRYEH8MYJ1');`,
+        }}
+      />
     </>
   );
 }
