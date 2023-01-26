ddf14358b65a8096753b482f39fd1263e14f91cf
Matt Montgomery <mmontgomery@deseretdigital.com>
2023-01-25 17:13:50 -0700

    fix: fixes authors not being comma-separated

--- frontend/src/app/components/Post/Author.tsx
+++ frontend/src/app/components/Post/Author.tsx
@@ -1,21 +1,23 @@
 import Link from "next/link";
+import styles from "@/app/styles/post.module.css";
 
 export default function Authors(props: App.Post) {
   const authors = props.attributes.authors?.data;
 
   return authors.length ? (
     <>
-      <span>
+      <span className={styles.commaSeparatedList}>
         by{" "}
         {authors.map((author, idx) => (
-          <Link
-            prefetch={false}
-            href={`/authors/${author.id}/${author.attributes.slug}`}
-            rel="author"
-            key={idx}
-          >
-            {author.attributes.name}
-          </Link>
+          <span key={idx}>
+            <Link
+              prefetch={false}
+              href={`/authors/${author.id}/${author.attributes.slug}`}
+              rel="author"
+            >
+              {author.attributes.name}
+            </Link>
+          </span>
         ))}
       </span>
     </>
