e4b8d0f5c9bf228e87e241d1c314e54bd9d9b6c9
Matt Montgomery <mmontgomery@deseretdigital.com>
2023-01-27 16:14:34 -0700

    feat: adds twitter embed support

--- frontend/src/app/components/Post/Embed.tsx
+++ frontend/src/app/components/Post/Embed.tsx
@@ -1,20 +1,29 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import useSWR from "swr";
 import styles from "@/app/styles/post.module.css";
+
+import { TwitterEmbed } from "react-social-media-embed";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export function Embed({ url }: { url: string }): React.ReactElement {
   const { data, error, isLoading } = useSWR(`/api/embed?url=${url}`, fetcher);
 
-  return isLoading || error || !data ? (
-    <p>{url}</p>
-  ) : (
+  if (isLoading || error || !data) {
+    return <p>{url}</p>;
+  }
+  if (data.data.provider_name === "Twitter") {
+    return (
+      <div className={styles.embed}>
+        <TwitterEmbed url={data.data.url} />
+      </div>
+    );
+  }
+  return (
     <div
       className={styles.embed}
-      dangerouslySetInnerHTML={{ __html: data?.data?.html }}
+      dangerouslySetInnerHTML={{ __html: data.data?.html }}
     />
   );
 }
