575388dfa5395a2f2bb455831cabc312ffd3379b
Matt Montgomery <mmontgomery@deseretdigital.com>
2023-02-02 10:24:15 -0700

    fix: removes ga from improper place

--- frontend/src/app/head.tsx
+++ frontend/src/app/head.tsx
@@ -1,7 +1,5 @@
-import Config, { getConfig } from "@/app/util/config";
-import Script from "next/script";
-import DefaultTags from "./DefaultTags";
-import { getSiteTitle, getTitle } from "./util/site";
+import { getConfig } from "@/app/util/config";
+import { getTitle } from "./util/site";
 
 export default async function Head() {
   const config = await getConfig();
@@ -9,28 +7,6 @@ export default async function Head() {
     <>
       <title>{await getTitle([config.homepageTitleText], true)}</title>
       <meta name="description" content={config.siteDescription} />
-      <link
-        rel="alternate"
-        type="application/rss+xml"
-        title={`RSS feed for ${config.siteName}`}
-        href="/rss.xml"
-      />
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-PRYEH8MYJ1"
-        strategy="afterInteractive"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-PRYEH8MYJ1');`,
-        }}
-      />
     </>
   );
 }
