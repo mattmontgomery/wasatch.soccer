c8031d951ee0b3687772fe5f6d0158aa23c49f7a
Matt Montgomery <montgomery.matt@gmail.com>
2023-05-30 14:09:55 -0600

    chore: dependency updates

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,4 +1,6 @@
-export default function ({ env }) {
+import type { PluginOptions } from "@strapi/strapi";
+
+export default function ({ env }): PluginOptions {
   return {
     publisher: {
       enabled: true,
@@ -56,11 +58,13 @@ export default function ({ env }) {
       config: {
         provider: "aws-s3",
         providerOptions: {
-          accessKeyId: env("AWS_ACCESS_KEY_ID"),
-          secretAccessKey: env("AWS_ACCESS_SECRET"),
-          region: env("AWS_REGION"),
-          params: {
-            Bucket: env("AWS_BUCKET"),
+          s3Options: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+            region: env("AWS_REGION"),
+            params: {
+              Bucket: env("AWS_BUCKET"),
+            },
           },
         },
         actionOptions: {
