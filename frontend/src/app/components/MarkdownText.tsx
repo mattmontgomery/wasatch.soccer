457dddaea4af039327c0ff675e9832afee5814f2
Matt Montgomery <montgomery.matt@gmail.com>
2023-07-08 15:57:20 -0600

    feat adds rehypeRaw to embed

--- frontend/src/app/components/MarkdownText.tsx
+++ frontend/src/app/components/MarkdownText.tsx
@@ -2,6 +2,7 @@ import ReactMarkdown, { Options } from "react-markdown";
 import remarkGfm from "remark-gfm";
 
 import styles from "./markdownText.module.css";
+import rehypeRaw from "rehype-raw";
 
 export default function MarkdownText(
   props: {
@@ -10,7 +11,11 @@ export default function MarkdownText(
 ): React.ReactElement {
   return (
     <div className={styles.markdownText}>
-      <ReactMarkdown {...props} remarkPlugins={[remarkGfm]} />
+      <ReactMarkdown
+        {...props}
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+      />
     </div>
   );
 }
