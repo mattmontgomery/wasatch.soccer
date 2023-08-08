001204e1ea0c9e96b4bdfcdc306f3e98c357faaa
Matt Montgomery <montgomery.matt@gmail.com>
2023-08-08 10:28:57 -0600

    fix: fixes live blog issues

--- frontend/src/app/post/[date]/[id]/[slug]/LiveBlog.tsx
+++ frontend/src/app/post/[date]/[id]/[slug]/LiveBlog.tsx
@@ -4,21 +4,33 @@ import { useEffect } from "react";
 
 export default function LiveBlog(props: { id?: string }): React.ReactElement {
   useEffect(() => {
+    const script = document.createElement("script");
+    script.src = `https://v.24liveblog.com/24.js`;
+    script.async = true;
+    document.body.appendChild(script);
     setTimeout(() => {
-      if (!document.querySelector("#LB24")) {
-        location.reload();
-      }
-    }, 5000);
+      window.dispatchEvent(new Event("load"));
+    }, 100);
+    return () => {
+      script.remove();
+      document.body
+        .querySelector("[src='https://cdn.24liveblog.com/live-js/js/24.js']")
+        ?.remove();
+      document.body
+        .querySelector("[src='https://chat.24liveblog.com/chat.js']")
+        ?.remove();
+      document.body
+        .querySelector(
+          "[src='https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.26.0/polyfill.min.js']"
+        )
+        ?.remove();
+    };
   }, [props.id]);
   return !props.id ? (
     <></>
   ) : (
     <div key={props.id}>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `<div id="LB24_LIVE_CONTENT" data-eid=${`${props.id}`} /><script async src="https://v.24liveblog.com/24.js"></script>`,
-        }}
-      />
+      <div id="LB24_LIVE_CONTENT" data-eid={props.id} />
     </div>
   );
 }
